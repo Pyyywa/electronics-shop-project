f55fad700d02208fc9153716b871ab144a334507
Rusha <pyyywa@gmail.com>
2024-03-14 18:27:05 +0300

    homework6- done

--- src/item.py
+++ src/item.py
@@ -2,6 +2,16 @@ import csv
 import os
 from pathlib import Path
 
+
+class InstantiateCSVError(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.message
+
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -71,17 +81,22 @@ class Item:
         self.price = self.price * self.pay_rate
 
     @classmethod
-    def instantiate_from_csv(cls, file: str) -> None:
-        cls.all = []
-        base_path = Path('../electronics-shop-project/').resolve().parent
-        file_path = base_path / file
-        with open(file_path, mode = 'r', newline = '', encoding = 'windows-1251') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                name = row['name']
-                price = float(row['price'])
-                quantity = int(row['quantity'])
-                cls(name, price, quantity)
+    def instantiate_from_csv(cls, file):
+        try:
+            cls.all = []
+            base_path = Path('../electronics-shop-project/').resolve().parent
+            file_path = base_path / file
+            with open(file_path, mode = 'r', newline = '', encoding = 'windows-1251') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    name = row['name']
+                    price = float(row['price'])
+                    quantity = int(row['quantity'])
+                    cls(name, price, quantity)
+        except FileNotFoundError:
+            print("Отсутствует файл item.csv")
+        except KeyError:
+            raise InstantiateCSVError()
 
 
     @staticmethod
